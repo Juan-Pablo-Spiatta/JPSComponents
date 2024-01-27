a8270fe48095798a4358878d4d84a776d656c5ef
Juan Pablo Spiatta <spiattajuanpablodesarrollador@gmail.com>
2024-01-27 19:19:38 -0300

    Se configuro para el correcto funcionamiento como libreria de componentes #1

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
@@ -10,6 +11,27 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  build: {
+    lib: {
+      // src/indext.ts is where we have exported the component(s)
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "JPSLibraryComponents",
+      // the name of the output files when the build is run
+      fileName: "jps-library-components",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
